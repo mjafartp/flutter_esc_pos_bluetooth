d93eaac962a916501e14624196935b2964ce3ab7
Jafar TP <jafar@lithos.in>
2021-08-19 07:35:59 +0530

    ghjfhjf

--- android/src/main/java/in/lithos/flutter_esc_pos_bluetooth/FlutterEscPosBluetoothPlugin.java
+++ android/src/main/java/in/lithos/flutter_esc_pos_bluetooth/FlutterEscPosBluetoothPlugin.java
@@ -329,9 +329,9 @@ public class FlutterEscPosBluetoothPlugin implements MethodCallHandler, RequestP
 
     public void write(byte[] bytes) {
       try {
-        int len;
+        int len = bytes.length;
 //        byte[] buffer = new byte[8192];
-        while ((len = inputStream.read(bytes)) != -1) {
+        while (true) {
           outputStream.write(bytes, 0, len);
         }
       } catch (IOException e) {
